d58ddaf7f61a69d9afb1161e7f9179d2121d3def
阿尔格 <846094423@qq.com>
2021-08-10 07:38:33 +0000

    update src/main/java/GitTest.java.

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!3");
         System.out.println("master test!");
         System.out.println("gitee test");
+        System.out.println("gitee test2");
     }
 }
